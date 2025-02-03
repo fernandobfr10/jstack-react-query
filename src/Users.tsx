811728c2b6b93b3cff58dbea2116f699d943b868
Fernando Junior <fernandobfr10@gmail.com>
2025-02-03 16:12:36 -0300

    using isLoading, isPending and isFetching and enabled option

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,18 +1,36 @@
 import { useQuery } from "@tanstack/react-query"
 
-import { IUser } from "./types"
+import { IUser } from "./types/types"
+import { sleep } from "./utils/sleep"
+
+// isLoading (boolean) => Será true quando a query function estiver executando na primeira requisição, deveria ser isInitialLoading
+// isPending (boolean) => Será true quando não houver nenhum valor no cache
+// isFetching (boolean) => Será true sempre que a query function estiver executando, seja na primeira execução ou nas subsequentes
 
 export function Users() {
-  const {data: users} = useQuery({
+  const {data: users, isLoading: isUsersInitialLoading, isFetching: isUsersFetching, refetch: refetchUsers} = useQuery({
     queryKey: ['users'],
+    enabled: false,
     queryFn: async (): Promise<IUser[]> => {
+      await sleep()
       const response = await fetch('http://localhost:3000/users')
       return response.json()
     },
   })
 
   return (
-    <div>
+    <div className="p-4">
+      <button
+        type="button"
+        className="bg-white text-black px-4 py-2 rounded-lg cursor-pointer"
+        onClick={() => refetchUsers()}
+      >
+        Listar usuários
+      </button>
+
+      {isUsersInitialLoading && <h2>Carregando...</h2>}
+      {!isUsersInitialLoading && isUsersFetching && <small className="block">Fetching...</small>}
+
       {users?.map(user => (
         <div key={user.id}>
           <strong className='block'>{user.name}</strong>
