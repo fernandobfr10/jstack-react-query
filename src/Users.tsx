60068feb6b02081913aca6c443a0a253dff2e235
Fernando Junior <fernandobfr10@gmail.com>
2025-02-04 11:17:59 -0300

    using error, isError, retry and retryDelay

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 
 import { IUser } from "./types/types"
+
 import { sleep } from "./utils/sleep"
 
 // isLoading (boolean) => Será true quando a query function estiver executando na primeira requisição, deveria ser isInitialLoading
@@ -13,12 +14,19 @@ import { sleep } from "./utils/sleep"
 // refetchOnWindowFocus (boolean) => Se true, a query será automaticamente refetchada quando a janela do navegador ganhar foco
 // refetchOnInterval (number) => Se definido, a query será automaticamente refetchada a cada X milissegundos
 
+// error (Error) => Será um objeto Error caso ocorra um erro na execução da query function
+// isError (boolean) => Será true se ocorrer um erro na execução da query function
+
+// retry (number | boolean) => Define a quantidade de vezes que a query será retentada em caso de erro, ou se true, a query será retentada indefinidamente (default 3)
+// retryDelay (number) => Define o tempo em milissegundos que a query será retentada em caso de erro
+
 export function Users() {
-  const {data: users, isLoading: isUsersInitialLoading, isFetching: isUsersFetching, refetch: refetchUsers} = useQuery({
+  const { data: users, isLoading: isUsersInitialLoading, isFetching: isUsersFetching, refetch: refetchUsers, error: errorUsers } = useQuery({
     queryKey: ['users'],
     enabled: false,
     refetchOnWindowFocus: false,
     queryFn: async (): Promise<IUser[]> => {
+      // throw new Error('Erro ao buscar usuários')
       await sleep()
       const response = await fetch('http://localhost:3000/users')
       return response.json()
@@ -37,6 +45,7 @@ export function Users() {
 
       {isUsersInitialLoading && <h2>Carregando...</h2>}
       {!isUsersInitialLoading && isUsersFetching && <small className="block">Fetching...</small>}
+      {errorUsers && <h2 className="text-red-400">{errorUsers.message}</h2>}
 
       {users?.map(user => (
         <div key={user.id}>
